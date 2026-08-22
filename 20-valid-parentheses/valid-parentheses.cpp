91b44ff1608dad8b3a31be37820e9a49efc7ba13
Sainki_Shiromani_07 <shainkiindian@gmail.com>
2026-08-23 01:00:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.87%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack <char> st;
         int n = s.length();
-        string st = "";
-        int i, stn;
-        bool b;
+        int i;
 
         for(i = 0; i<n; i++){
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                st += s[i];
-                stn = st.length();
+               st.push(s[i]);
             }
             else{
-                if(st.empty()){
+                if (st.empty()) {
                     return false;
                 }
-
-                if(s[i] != map<char, char>{{'(', ')'}, {'{', '}'}, {'[', ']'}}[st.back()]){
+                if ((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')) {
+                    st.pop();
+                }
+                else{
                     return false;
                 }
-
-                st.pop_back();
             }
         }
-
-        if(st == ""){
-            b = true;
-        }
-        else{
-            b = false;
-        }
-        return b;
+        return st.empty();
     }
 };
